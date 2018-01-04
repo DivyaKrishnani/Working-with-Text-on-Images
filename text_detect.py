42b12e38b24b98dd8257dfba5516264b051db705
Divya Krishnani <div.krish02@gmail.com>
2018-01-04 19:34:36 +0530

    Add files via upload

---
+++ text_detect.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+large = cv2.imread('images/img.png')
+
+small = cv2.cvtColor(large, cv2.COLOR_BGR2GRAY)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+grad = cv2.morphologyEx(small, cv2.MORPH_GRADIENT, kernel)
+
+_, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+
+contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+mask = np.zeros(bw.shape, dtype=np.uint8)
+
+for idx in range(len(contours)):
+    x, y, w, h = cv2.boundingRect(contours[idx])
+    mask[y:y+h, x:x+w] = 0
+    cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+    r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+
+    if r > 0.45 and w > 8 and h > 8:
+        cv2.rectangle(large, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+
+cv2.imshow('rects', large)
+k = cv2.waitKey(0) & 0xFF
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('images/detected_text.jpg', large)
+    cv2.destroyAllWindows()
